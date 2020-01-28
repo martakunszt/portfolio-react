b48e31d12c64ad850a463c2375b2b4ce3d1e0193
martakunszt <kunsztowicz.m@gmaila.com>
2020-01-28 10:59:12 +0000

    Updates

---
+++ precache-manifest.54991d8a7c497edc64e1af75842f58e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "277a74fc2f0ec078beff21694111fc96",
+    "url": "/martakunszt/portfolio-react/build/index.html"
+  },
+  {
+    "revision": "ce1d488702445d3b25a2",
+    "url": "/martakunszt/portfolio-react/build/static/css/main.bb0778d6.chunk.css"
+  },
+  {
+    "revision": "5a5dd13fb5e1f5eeb377",
+    "url": "/martakunszt/portfolio-react/build/static/js/2.f175dcd4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/martakunszt/portfolio-react/build/static/js/2.f175dcd4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ce1d488702445d3b25a2",
+    "url": "/martakunszt/portfolio-react/build/static/js/main.28a8db67.chunk.js"
+  },
+  {
+    "revision": "a578e5a539e09d6d9938",
+    "url": "/martakunszt/portfolio-react/build/static/js/runtime-main.27a1e4b5.js"
+  }
+]);
